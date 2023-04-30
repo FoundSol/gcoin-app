0a4eaddb304ead4c9ba8e241136c9f6f768cf2b6
Ming Zhu <zhuming0828@fb.com>
2023-04-30 18:59:53 -0400

    inital testing and setup a framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MintGCoin from './components/MintGCoin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container mt-5">
+        <MintGCoin />
+        {/* Add other tabs or components later, this is just demo?  */}
+      </div>
     </div>
   );
 }
